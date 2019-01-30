d87156d8cc5f7ad3c29d0e3cda177523dbcdaaca
Frank Diaz <diazfrankp@gmail.com>
2019-01-30 17:22:18 -0500

    Models in progress.

---
+++ Properties/Models/Dealer.cs
@@ -0,0 +1,19 @@
+namespace carsearchapi.Models
+{
+  class Dealer
+  {
+    public int Id { get; set; }
+
+    public string MyProperty { get; set; }
+
+    public string Address { get; set; }
+
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+    public string Zip { get; set; }
+
+    public string PhoneNumber { get; set; }
+  }
+}
